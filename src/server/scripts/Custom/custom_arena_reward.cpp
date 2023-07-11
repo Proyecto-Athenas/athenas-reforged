8a7b8229d3625bd8e6f0f9b89b10e49da1240d49
Jose Joya <ing.jose.joya@gmail.com>
2023-07-11 01:20:45 -0500

    chore: Added Lite documentation about the custom hook

--- src/server/scripts/Custom/custom_arena_reward.cpp
+++ src/server/scripts/Custom/custom_arena_reward.cpp
@@ -15,6 +15,7 @@ class Custom_Arena_Reward : public PlayerScript
 public:
     Custom_Arena_Reward() : PlayerScript("Custom_Arena_Reward") {}
 
+    // Count the item when is received in the inventory, only if you are in arena and the Item is exactly the Bloody badge.
     void OnAddItem(Player *player, Item *item)
     {
         if (player->InArena() && item->GetEntry() == ITEM_BLOODY_BADGE)
@@ -25,15 +26,17 @@ public:
 
     void OnPVPKill(Player *killer, Player *killed)
     {
-
+        // Check if the player is in arena and if is available to receive a reward
         if (killer->InArena() && itemsRewarded < MAX_REWARD)
         {
+            // Share with all the arena team the reward.
             if (Group *group = killer->GetGroup())
             {
                 for (GroupReference *groupRef = group->GetFirstMember(); groupRef != NULL; groupRef->next())
                 {
                     if (Player *teamMember = groupRef->getSource())
                     {
+                        // Send The reward to the team if the team member is on the Arena.
                         if (teamMember->InArena())
                             teamMember->AddItem(ITEM_BLOODY_BADGE, 1);
                     }
@@ -42,11 +45,14 @@ public:
         }
     }
 
+    // Set The SCript Limits whe the player enter into the arena, The idea is to fix the Limit accordingly to the Arena Team Type, for instance, if the player enter into the 2vs2 arena, the limit is set to 2.
     void OnEnterMap(Player *player, uint32 /*mapId*/)
     {
         if (player->InArena())
         {
+            // Setup the Battleground Pointer
             currentBattleground = player->GetBattleground();
+            // Setup Limits for the rewards
             MAX_REWARD = currentBattleground->GetArenaType();
             itemsRewarded = 0;
         }
