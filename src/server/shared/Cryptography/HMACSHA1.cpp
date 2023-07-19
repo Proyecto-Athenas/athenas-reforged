778a458013dae9fc77cf7d3ecf813f6c55d5ae84
Jose Joya <ing.jose.joya@gmail.com>
2023-07-19 11:17:41 -0500

    Fix: Missing code fixing

--- src/server/shared/Cryptography/HMACSHA1.cpp
+++ src/server/shared/Cryptography/HMACSHA1.cpp
@@ -21,14 +21,14 @@
 #include "Common.h"
 
 #if defined(OPENSSL_VERSION_NUMBER) && OPENSSL_VERSION_NUMBER < 0x10100000L
-HMAC_CTX* HMAC_CTX_new()
+HMAC_CTX *HMAC_CTX_new()
 {
-    HMAC_CTX* ctx = new HMAC_CTX();
+    HMAC_CTX *ctx = new HMAC_CTX();
     HMAC_CTX_init(ctx);
     return ctx;
 }
 
-void HMAC_CTX_free(HMAC_CTX* ctx)
+void HMAC_CTX_free(HMAC_CTX *ctx)
 {
     HMAC_CTX_cleanup(ctx);
     delete ctx;
@@ -39,7 +39,7 @@ HmacHash::HmacHash(uint32 len, uint8 *seed)
 {
     m_ctx = HMAC_CTX_new();
     HMAC_Init_ex(m_ctx, seed, len, EVP_sha1(), nullptr);
-    memset(m_digest, 0, sizeof(m_digest))
+    memset(m_digest, 0, sizeof(m_digest));
 }
 
 HmacHash::~HmacHash()
@@ -47,12 +47,12 @@ HmacHash::~HmacHash()
     HMAC_CTX_free(m_ctx);
 }
 
-void HmacHash::UpdateData(std::string const& str)
+void HmacHash::UpdateData(std::string const &str)
 {
-    HMAC_Update(m_ctx, reinterpret_cast<uint8 const*>(str.c_str()), str.length());
+    HMAC_Update(m_ctx, reinterpret_cast<uint8 const *>(str.c_str()), str.length());
 }
 
-void HmacHash::UpdateData(uint const* data, size_t len)
+void HmacHash::UpdateData(const uint8 *data, size_t len)
 {
     HMAC_Update(m_ctx, data, len);
 }
@@ -64,9 +64,9 @@ void HmacHash::Finalize()
     ASSERT(length == SHA_DIGEST_LENGTH)
 }
 
-uint8* HmacHash::ComputeHash(BigNumber* bn)
+uint8 *HmacHash::ComputeHash(BigNumber *bn)
 {
-    HMAC_Update(m_ctx, bn->AsByteArray().get(), bn->GetNumBytes());
+    HMAC_Update(m_ctx, bn->AsByteArray(), bn->GetNumBytes());
     Finalize();
     return m_digest;
 }
