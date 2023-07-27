caad2b4b9681abfe20cd9005ddc8dc7570df9794
Angelo Venturini <nefertum.dev@protonmail.com>
2023-07-27 16:51:55 -0300

    fix(Core/Spells): Glyph of Resilient Grip (#13)
    * fix(Core/Spells): Glyph of Resilient Grip
    
    * comment

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -4403,7 +4403,8 @@ void Spell::EffectScriptEffect(SpellEffIndex effIndex)
             {
                 case 90289: // Glyph of resilient grip
                     if (m_caster->GetTypeId() == TYPEID_PLAYER)
-                        m_caster->ToPlayer()->RemoveSpellCooldown(49576, true);
+                        // Athenas: instead of removing the cd, we set it to 1 sec
+                        m_caster->ToPlayer()->ModifySpellCooldown(49576, -34000);
                     break;
             }
             // Pestilence
